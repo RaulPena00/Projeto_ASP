075f6ef5585fae3b472900bb850ba11e67ff5ba0
Raul Pena Pereira <raulppereira00@gmail.com>
2023-08-16 15:13:13 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_ASP/Data/FuncionarioDep.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_ASP.Models;
+
+namespace Projeto_ASP.Data
+{
+    public class FuncionarioDep : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-UQQF9JL; Database=ProjetoDep; User Id=ra; Password=ra12345; TrustServerCertificate=True");
+        }
+
+    }
+}
---
+++ Projeto_ASP/Models/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿namespace Projeto_ASP.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Foto { get; set; } // REVISAR
+        public int RG { get; set; }
+        public Departamento Departamento { get; set; }
+        public int DepartamentoId { get; set; }
+
+    }
+}
